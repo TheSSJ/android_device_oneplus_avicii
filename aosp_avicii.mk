f71a95fdde9da6c7a796fa68fc24076817a79ee6
Sanju0910 <sreeshankar0910@gmail.com>
2023-03-22 10:45:41 +0530

    avicii: Bring up Elixir

--- aosp_avicii.mk
+++ aosp_avicii.mk
@@ -12,26 +12,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/avicii/device.mk)
 
 # Inherit some common riceDroid stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Additional stuff for this product.
+# Add Elixir Flags
+ELIXIR_MAINTAINER := Sreeshankar K
 TARGET_BOOT_ANIMATION_RES := 1080
-RICE_MAINTAINER := Sreeshankar K
-RICE_CHIPSET := Snapdragon 765G 5G
-RICE_PACKAGE_TYPE := PIXEL
-TARGET_BUILD_APERTURE_CAMERA := true
-SUSHI_BOOTANIMATION := 1080
-WITH_GMS := true
-TARGET_USE_GOOGLE_TELEPHONY := true
-TARGET_USES_CUSTOM_AVB_KEY := true
 TARGET_SUPPORTS_QUICK_TAP := true
-WITH_FOD_ANIMATIONS := true
-TARGET_HAS_UDFPS := true
-TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
-TARGET_USES_BLUR := true   
+TARGET_SUPPORTS_CALL_RECORDING := true
+EXTRA_UDFPS_ANIMATIONS := true
 
 # Device specific
-PRODUCT_NAME := lineage_avicii
+PRODUCT_NAME := aosp_avicii
 PRODUCT_DEVICE := avicii
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
